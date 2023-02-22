32e5ec6e36f1e1a07f2b446eb318e78502bbb8d3
Manas-pi-engg <122532804+Manas-pi-engg@users.noreply.github.com>
2023-02-22 18:02:24 +0530

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,11 +2,9 @@ public class Calculator {
   public static void main(String[] args) {
     int num1 = 10;
     int num2 = 5;
-    
     int sum = add(num1, num2);
     int difference = subtract(num1, num2);
     int product = multiply(num1, num2);
-    
     System.out.println(sum+difference +product);
   }
   
